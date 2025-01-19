2cef947f92728deb578b752554cae8693dc436be
ajeglon <61421392+ajeglon@users.noreply.github.com>
2025-01-19 15:16:22 +0000

    created if scenario for greet to output good morning

--- greeter.js
+++ greeter.js
@@ -2,7 +2,16 @@ class Greeter {
     greet(name) {
         const trimmedName = name.trim();
         const capitalisedName = trimmedName.charAt(0).toUpperCase() + trimmedName.slice(1);
-        const greeting = `Hello ${capitalisedName}`;
+        const currentTime = new Date();
+        const hours = currentTime.getHours();
+        let greeting;
+
+        if (hours >= 6 && hours < 12) {
+            greeting = `Good morning ${capitalisedName}`;
+        } else {
+            greeting = `Hello ${capitalisedName}`;
+        }
+        
         return greeting;
     }
 }
