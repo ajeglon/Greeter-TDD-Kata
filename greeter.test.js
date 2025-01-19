4ba309dc7622b5544fd7196093d57285d40d8e0c
ajeglon <61421392+ajeglon@users.noreply.github.com>
2025-01-19 16:02:29 +0000

    updated test 1,2 and 3 to have a specific time called as depending on what time tested, these tests could fail if not set to a time in the else conditions of the statment between 12 to 18

--- greeter.test.js
+++ greeter.test.js
@@ -6,66 +6,77 @@ describe('Greeter', () => {
 
     //1. Write a Greeter class with greet function that receives a name as input and outputs Hello <name> . The signature of greet should not change throughout the kata. You are allowed to construct Greeter object as you please.
     test('should greet Hello <name>', () => {
-        const result = greeter.greet('Anthony');
-        expect(result).toBe('Hello Anthony');
-    });
-    //2. greet trims the input
-    test('should greet Hello <name> while trimming whitespace', () => {
-        const result = greeter.greet('      Anthony     ');
-        expect(result).toBe('Hello Anthony');
-    });
-
-    //3. greet capitalizes the first letter of the name
-    test('should greet Hello <name> with capital letter of first letter', () => {
-        const result = greeter.greet('      anthony     ');
-        expect(result).toBe('Hello Anthony');
-    });
-
-    //4. greet returns Good morning <name> when the time is 06:00-12:00
-    test('should return "Good morning" between 06:00 and 12:00', () => {
         // Store data as a variable before mocking it
         const RealDate = Date;
         // Mock the date 
         global.Date = jest.fn(() => {
             // Return the mocked date
+            return new RealDate('2025-01-19T015:00:00Z');
+        });
+    
+        const result = greeter.greet('Anthony');
+        expect(result).toBe('Hello Anthony');
+        // restores original Date constructor to avoid side effects on other tests
+        global.Date = RealDate; 
+
+    });
+    //2. greet trims the input
+    test('should greet Hello <name> while trimming whitespace', () => {
+        const RealDate = Date;
+        global.Date = jest.fn(() => {
+            return new RealDate('2025-01-19T015:00:00Z');
+        });
+    
+        const result = greeter.greet('      Anthony     ');
+        expect(result).toBe('Hello Anthony');
+        global.Date = RealDate; 
+    });
+
+    //3. greet capitalizes the first letter of the name
+    test('should greet Hello <name> with capital letter of first letter', () => {
+        const RealDate = Date;
+        global.Date = jest.fn(() => {
+            return new RealDate('2025-01-19T015:00:00Z');
+        });
+
+        const result = greeter.greet('      anthony     ');
+        expect(result).toBe('Hello Anthony');
+        global.Date = RealDate;
+    });
+
+    //4. greet returns Good morning <name> when the time is 06:00-12:00
+    test('should return "Good morning" between 06:00 and 12:00', () => {
+        const RealDate = Date;
+        global.Date = jest.fn(() => {
             return new RealDate('2025-01-19T09:00:00Z');
         });
     
         const result = greeter.greet('      anthony     ');
         expect(result).toBe('Good morning Anthony');
-        // restores original Date constructor to avoid side effects on other tests
         global.Date = RealDate; 
     });
 
     //5. greet returns Good evening <name> when the time is 18:00-22:00
     test('should return "Good evening" between 18:00 and 22:00', () => {
-        // Store data as a variable before mocking it
         const RealDate = Date;
-        // Mock the date 
         global.Date = jest.fn(() => {
-            // Return the mocked date
             return new RealDate('2025-01-19T20:00:00Z');
         });
     
         const result = greeter.greet('      anthony     ');
         expect(result).toBe('Good evening Anthony');
-        // restores original Date constructor to avoid side effects on other tests
         global.Date = RealDate; 
     });
 
     //6. greet returns Good night <name> when the time is 22:00-06:00
     test('should return "Good night" between 22:00 and 06:00', () => {
-        // Store data as a variable before mocking it
         const RealDate = Date;
-        // Mock the date 
         global.Date = jest.fn(() => {
-            // Return the mocked date
             return new RealDate('2025-01-19T02:00:00Z');
         });
     
         const result = greeter.greet('      anthony     ');
         expect(result).toBe('Good night Anthony');
-        // restores original Date constructor to avoid side effects on other tests
         global.Date = RealDate; 
     });
 
